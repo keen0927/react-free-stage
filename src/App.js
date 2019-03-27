93d33b0ab66bc2506cdc333f8c9626536a2fb7eb
KEEN <kyh0927@live.co.kr>
2019-03-28 00:16:12 +0900

    router animate test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
-
 import Home from './pages/Home';
-import Posts from './pages/Posts';
 import About from './pages/About';
+
+import { CSSTransition } from 'react-transition-group';
+
+import './App.css';
 
 export class App extends Component {
   render() {
     return (
       <BrowserRouter>
           <div>
-              <p><Link to={`/`}>홈으로</Link></p>
-              <p><Link to={`/posts`}>포스트로</Link></p>
+              <p><Link to={`/Home`}>Home</Link></p>
               <p><Link to={`/about`}>About</Link></p>
           </div>
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route path="/about" component={About}/>
-            <Route path="/posts" component={Posts}/>
+            <Route path="/Home" component={Home}/>
+              <CSSTransition
+                  in={true}
+                  timeout={300}
+                  classNames="page"
+                  unmountOnExit
+              >
+                  <div className="page">
+                    <Route path="/about" component={About}/>
+                  </div>
+              </CSSTransition>
           </Switch>
-
       </BrowserRouter>
 
     )
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,20 +1,11 @@
-import React from 'react'
-import { CSSTransitionGroup } from 'react-transition-group'
-import './transition.css'
+import React from 'react';
 
-const Works = () => {
+const About = () => {
     return (
-        <CSSTransitionGroup
-            transitionName="worksTransition"
-            transitionAppear={true}
-            transitionAppearTimeout={500}
-            transitionEnter={false}
-            transitionLeave={false}>
-            <div>
-                Works
-            </div>
-        </CSSTransitionGroup>
-    )
-}
+        <div>
+           ABOUT
+        </div>
+    );
+};
 
-export default Works
+export default About;
